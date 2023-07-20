3caa61452e1e86cf39a85e1b699f7b96279593e4
GiulioDangelo <dangelogiulio48@gmail.com>
2023-07-20 18:54:47 +0200

    add mail

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -1,11 +1,90 @@
+<script>
+import axios from "axios"
+import { store } from "../store"
+
+  export default{
+    data() {
+      return {
+        store,
+        name: '',
+        email: '',
+        message: '',
+        newsletter: true,
+        showSuccess: false,
+        isSending: false,
+        showError: false,
+        errors: {},
+      }
+    },
+
+    methods: {
+      sendLead(){
+        axios
+				.post(this.store.baseUrl + 'api/leads', {
+					name: this.name,
+					email: this.email,
+					message: this.message,
+					newsletter: this.newsletter,
+				})
+				.then(response => {
+					this.isSending = false;
+
+					if (response.data.success) {
+						this.showSuccess = true;
+						this.resetForm();
+					} else {
+						this.showError = true;
+						this.errors = response.data.errors;
+					}
+				});
+      },
+
+      resetForm(){
+        this.name = '';
+        this.email = '';
+        this.message = '';
+        this.newsletter = true;
+      }
+    },
+  }
+</script>
+
 <template>
+<div
+		v-if="showSuccess"
+		class="alert alert-success alert-dismissible fade show"
+		role="alert"
+	>
+		Messaggio inviato con successo
+		<button
+			type="button"
+			class="btn-close"
+			aria-label="Close"
+			@click="showSuccess = false"
+		></button>
+	</div>
+
+	<div
+		v-if="showError"
+		class="alert alert-danger alert-dismissible fade show"
+		role="alert"
+	>
+		Errore nell'invio del messaggio
+		<button
+			type="button"
+			class="btn-close"
+			aria-label="Close"
+			@click="showError = false"
+		></button>
+	</div>
+
   <div class="p-0 m-0 d-flex">
     <div class="left w-50">
       <img src="../../src/assets/img/contattaci3.avif" alt="" />
     </div>
     <div class="align-self-center w-50 p-5">
       <h1 class="text-center text-light">Contattaci</h1>
-      <form novalidate>
+      <form @submit.prevent="sendLead" novalidate>
         <div class="mb-3">
           <label for="name" class="form-label">Nome</label>
           <input
@@ -13,6 +92,7 @@
             class="form-control"
             id="name"
             placeholder="Inserisci il tuo nome"
+            v-model="name"
           />
         </div>
         <div class="mb-3">
@@ -21,6 +101,7 @@
             class="form-control"
             id="email"
             placeholder="Inserisci la tua email"
+            v-model="email"
           />
         </div>
         <div class="mb-3">
@@ -30,6 +111,7 @@
             id="message"
             rows="5"
             placeholder="Lasciaci un feedback!"
+            v-model="message"
           ></textarea>
         </div>
         <div class="mb-3 form-check">
