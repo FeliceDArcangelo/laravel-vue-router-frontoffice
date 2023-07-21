80391b1be40307b52801f94a407b74e7991ee626
GiulioDangelo <dangelogiulio48@gmail.com>
2023-07-21 11:50:35 +0200

    footer and contacts fix

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -78,9 +78,9 @@ import { store } from "../store"
 		></button>
 	</div>
 
-  <div class="p-0 m-0 d-flex">
+  <div class="p-0 m-0 d-flex content">
     <div class="left w-50">
-      <img src="../../src/assets/img/contattaci3.avif" alt="" />
+      <img src="https://images.unsplash.com/photo-1607622750671-6cd9a99eabd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80" alt="" />
     </div>
     <div class="align-self-center w-50 p-5">
       <h1 class="text-center text-light">Contattaci</h1>
@@ -127,6 +127,10 @@ import { store } from "../store"
 </template>
 
 <style lang="scss" scoped>
+.content{
+  height: calc(100vh - 126px)
+}
+
 img {
   width: 100%;
   height: 100%;
